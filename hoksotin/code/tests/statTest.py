c9d7d2273ba8f9b957d075932eb6bf61746ffb84
Atte Rautio <atte.m.rautio@student.jyu.fi>
2013-04-18 15:55:40 +0300

    Tein testeja
    Former-commit-id: 2d84809a0dace64c0cfe280cec8a63ef3fd2102a

--- hoksotin/code/tests/statTest.py
+++ hoksotin/code/tests/statTest.py
@@ -2,32 +2,111 @@
 """
 Created on Mar 14, 2013
 
-@author: Jaakko Leppäkangas
+@author: Jaakko Leppäkangas, Atte Rautio
 """
 import unittest
-import numpy as np
+
 from statistic import Statistic
 
-class Test(unittest.TestCase):
-
-
-    def test_max(self):
-        """
-        Tests for find_maximum
-        """
-        a = [0,25,7,5,48,6,84,2,1]
-        b = [0,85,85,5,48,6,85,48]
-        c = ['a','b']
-        d = [-40, -100, 0, -15]
+class TestStatistic(unittest.TestCase):
+    """
+    Unit tests for the Statistic-class.
+    """
+    sfreq_int = 1
+    sfreq_double = 1.5
+    sfreq_zero = 0
+        
+    arr_normal = [0,25,7,5,48,6,84,2,1]
+    arr_many_maximums = [0,85,85,5,48,6,85,48]
+    arr_chars = ['a','b']
+    arr_negatives = [-40, -100, 0, -15]
+    arr_const = [2,2,2,2]
+    arr_empty = []
+        
+    tmin_int = 1000
+    tmin_double = 1000.5
+    tmin_negative = -6000
+    tmin_large = 8000
+        
+    tmax_int = 5000
+    tmax_double = 5000.5
+    tmax_negative = -1000
+    tmax_small = 1000
+    
+    ex_negative_tmin_tmax = 'Negative values for tmin and tmax not allowed.'
+    ex_sfreq_zero = 'Sampling frequency cannot be zero or negative.'
+    ex_arr_empty = 'No data found.'
+    ex_tmin_greater_than_tmax = 'tmax must be greater than tmin.'
+        
+    def test_all_normal(self):
         s = Statistic()
-        self.assertEqual(s.find_maximum(1, a, 1000, 5000), (48, 4000), 'Find_maximum1 failed')
-        # self.assertEqual(s.find_maximum(1, b, 1000, 5000), (85, 2000), 'Find_maximum2 failed')
-        self.assertEqual(s.find_maximum(1000, a), (84, 6), 'Find_maximum3 failed')
-        self.assertEqual(s.find_half_maximum(1000, a), (42, 6), 'Find_maximum4 failed')
-        self.assertEqual(s.find_maximum(0.5, a, 1000, 5000), (25,2000), 'Find_maximum5 failed')
-        # self.assertEqual(s.find_maximum(1, c, 1000, 1000), (85, 2000), 'Find_maximum6 failed')
-        self.assertEqual(s.find_maximum(1, d, 1000, 8000), (0, 2000), 'Find_maximum6 failed')
+        
+        self.assertEqual(s.find_maximum(self.sfreq_int, self.arr_normal,
+                                        1000, 5000), (48, 4000), 
+                                        'Find_maximum normal failed')
         pass
+    
+    def test_max_freq_double(self):
+        s = Statistic()
+        
+        self.assertEqual(s.find_maximum(self.sfreq_double, self.arr_normal,
+                                        1000,5000), (48, 4000),
+                                        'Find_maximum frequency double failed')
+        pass
+    
+    def test_max_freq_zero(self):
+        s = Statistic()
+        
+        with self.assertRaises(Exception) as e:
+            s.find_maximum(self.sfreq_zero, self.arr_normal, 1000, 5000)
+            
+        self.assertEqual(e.exception.message, self.ex_sfreq_zero,
+                         'Unexpected exception raised.')
+            
+        pass
+    
+    def test_max_tmin_double(self):
+        s = Statistic()
+        
+        self.assertEqual(s.find_maximum(self.sfreq_int, self.arr_normal,
+                                        self.tmin_double,self.tmax_int),
+                                        (48, 4000),
+                                         'Find_maximum tmin double failed')
+        pass
+    
+    def test_max_tmin_negative(self):
+        s = Statistic()
+        
+        with self.assertRaises(Exception) as e:
+            s.find_maximum(self.sfreq_int, self.arr_normal, 
+                           self.tmin_negative, self.tmax_int)
+            
+        self.assertEqual(e.exception.message, self.ex_negative_tmin_tmax,
+                         'Unexpected exception raised.')    
+        pass
+    
+    def test_max_tmax_negative(self):
+        s = Statistic()
+        
+        with self.assertRaises(Exception) as e:
+            s.find_maximum(self.sfreq_int, self.arr_normal,
+                           self.tmin_int, self.tmax_negative)
+            
+        self.assertEqual(e.exception.message, self.ex_negative_tmin_tmax,
+                         'Unexpected exception raised.')
+        pass
+    
+    def test_max_tmax_smaller_than_tmin(self):
+        s = Statistic()
+        
+        with self.assertRaises(Exception) as e:
+            s.find_maximum(self.sfreq_int, self.arr_normal,
+                           self.tmin_large, self.tmax_small)
+            
+        self.assertEqual(e.exception.message, self.ex_tmin_greater_than_tmax,
+                         'Unexpected exception raised.')
+        pass
+        """
 
     def test_min(self):
         a = [0,25,4,1,5,67,2,4,6,467,7]
@@ -35,7 +114,8 @@ class Test(unittest.TestCase):
         s = Statistic()
         self.assertEqual(s.find_minimum(1, a, 1000, 5000), (1,3000), 'Find_minimum failed')
         self.assertEqual(s.find_minimum2d(1000, m), [(4,0),(1,0),(7,0)], 'Find_minimum2d failed')
-
-if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testMax']
-    unittest.main()
+        """
+    if __name__ == "__main__":
+        #import sys;sys.argv = ['', 'TestStatistic.testMax']
+        unittest.main()
+    
