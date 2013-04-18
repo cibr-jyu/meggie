68114a087468b8edfa684bd53cd5903ac2c0c95d
Jaakko Leppäkangas <jaeilepp@sovproj08.it.jyu.fi>
2013-04-18 12:14:13 +0300

    ECG ja EOG projektiot
    Former-commit-id: 57b883cd1d6212f592b1859a19f9fd0bf6f65194

--- hoksotin/code/caller.py
+++ hoksotin/code/caller.py
@@ -46,8 +46,70 @@ class Caller(object):
         retval = proc.wait()
         print "the program return code was %d" % retval
         
-    def call_ecg_ssp(self):
-        pass
+    def call_ecg_ssp(self, dic):
+        raw_in = dic.get('i')
+        tmin = dic.get('tmin')
+        tmax = dic.get('tmax')
+        event_id = dic.get('event-id')
+        ecg_low_freq = dic.get('ecg-l-freq')
+        ecg_high_freq = dic.get('ecg-h-freq')
+        grad = dic.get('n-grad')
+        mag = dic.get('n-mag')
+        eeg = dic.get('n-eeg')
+        filter_low = dic.get('l-freq')
+        filter_high = dic.get('h-freq')
+        
+        rej_grad = dic.get('rej-grad')
+        rej_mag = dic.get('rej-mag')
+        rej_eeg = dic.get('rej-eeg')
+        rej_eog = dic.get('rej-eog')
+        
+        reject = dict(grad=1e-13 * float(rej_grad),
+                  mag=1e-15 * float(rej_mag),
+                  eeg=1e-6 * float(rej_eeg),
+                  eog=1e-6 * float(rej_eog))
+        flat = None
+        bads = [] #TODO: Check how the whole bads-thing is supposed to work.
+        
+        start = dic.get('tstart')
+        taps = dic.get('filtersize')
+        njobs = dic.get('n-jobs')
+        eeg_proj = dic.get('avg-ref')
+        excl_ssp = dic.get('no-proj')
+        comp_ssp = dic.get('average')
+        preload = True #TODO File
+        ch_name = dic.get('ch_name')
+        
+        if raw_in.endswith('_raw.fif') or raw_in.endswith('-raw.fif'):
+            prefix = raw_in[:-8]
+        else:
+            prefix = raw_in[:-4]
+        
+        ecg_event_fname = prefix + '_ecg-eve.fif'
+        
+        if comp_ssp:
+            ecg_proj_fname = prefix + '_ecg_avg_proj.fif'
+        else:
+            ecg_proj_fname = prefix + '_ecg_proj.fif'
+        
+        raw = mne.fiff.Raw(raw_in, preload=preload)
+        
+        projs, events = mne.preprocessing.compute_proj_ecg(raw, None,
+                            tmin, tmax, grad, mag, eeg,
+                            filter_low, filter_high, comp_ssp, taps,
+                            njobs, ch_name, reject, flat,
+                            bads, eeg_proj, excl_ssp, event_id,
+                            ecg_low_freq, ecg_high_freq, start)
+        raw.close()
+        
+        if isinstance(preload, basestring) and os.path.exists(preload):
+            os.remove(preload)
+        
+        print "Writing ECG projections in %s" % ecg_proj_fname
+        mne.write_proj(ecg_proj_fname, projs)
+        
+        print "Writing ECG events in %s" % ecg_event_fname
+        mne.write_events(ecg_event_fname, events)
     
     def call_eog_ssp(self, dic):
         #os.environ['MNE_ROOT'] = '/usr/local/bin/MNE-2.7.0-3106-Linux-x86_64' #TODO Remove
@@ -59,8 +121,8 @@ class Caller(object):
         tmin = dic.get('tmin')
         tmax = dic.get('tmax')
         event_id = dic.get('event-id')
-        low_freq = dic.get('eog-l-freq')
-        high_freq = dic.get('eog-h-freq')
+        eog_low_freq = dic.get('eog-l-freq')
+        eog_high_freq = dic.get('eog-h-freq')
         grad = dic.get('n-grad')
         mag = dic.get('n-mag')
         eeg = dic.get('n-eeg')
@@ -81,11 +143,12 @@ class Caller(object):
         eeg_proj = dic.get('avg-ref')
         excl_ssp = dic.get('no-proj')
         comp_ssp = dic.get('average')
-        
+        preload = True #TODO File
         reject = dict(grad=1e-13 * float(rej_grad), mag=1e-15 * (rej_mag),
                       eeg=1e-6 * float(rej_eeg), eog=1e-6 * (rej_eog))
         
-        if raw_in.endswith('_raw.fif') or raw_in.endswith('-raw.fif'):
+        if (raw_in.endswith('_raw.fif') 
+        or raw_in.endswith('-raw.fif')):
             prefix = raw_in[:-8]
         else:
             prefix = raw_in[:-4]
@@ -97,22 +160,28 @@ class Caller(object):
         else:
             eog_proj_fname = prefix + '_eog_proj.fif'
             
-        raw = mne.fiff.Raw(raw_in)
+        raw = mne.fiff.Raw(raw_in, preload=preload)
         
         projs, events = mne.preprocessing.compute_proj_eog(raw, None,
                             tmin, tmax, grad, mag, eeg,
-                            low_freq, high_freq, comp_ssp, taps,
+                            filter_low, filter_high, comp_ssp, taps,
                             njobs, reject, flat, bads,
                             eeg_proj, excl_ssp, event_id,
-                            filter_low, filter_high, start)
+                            eog_low_freq, eog_high_freq, start)
             
         raw.close()
+        
+        #TODO Reading a file
+        if isinstance(preload, basestring) and os.path.exists(preload):
+            os.remove(preload)
         
         print "Writing EOG projections in %s" % eog_proj_fname
         mne.write_proj(eog_proj_fname, projs)
         
         print "Writing EOG events in %s" % eog_event_fname
         mne.write_events(eog_event_fname, events)
+        
+        
         """
         bs = '$MNE_ROOT/bin/mne_compute_proj_eog.py '
         bs = 'python /usr/local/bin/mne-python-master/bin/mne_compute_proj_eog.py '
--- hoksotin/ui/ecgParametersDialog_main.py
+++ hoksotin/ui/ecgParametersDialog_main.py
@@ -5,7 +5,7 @@ Created on Apr 16, 2013
 """
 
 from PyQt4 import QtCore,QtGui
-from eogParametersDialog import Ui_Dialog
+from ecgParametersDialog_Ui import Ui_Dialog
 
 from caller import Caller
 
@@ -19,5 +19,54 @@ class EcgParametersDialog(QtGui.QDialog):
         self.ui.setupUi(self)
         
     def accept(self):
-        pass
+        dictionary = {'i': self.parent.experiment.raw_data.info.get('filename')}
+        tmin = self.ui.doubleSpinBoxTmin.value()
+        dictionary['tmin'] = tmin
+        tmax = self.ui.doubleSpinBoxTmax.value()
+        dictionary['tmax'] = tmax
+        event_id = self.ui.spinBoxEventsID.value()
+        dictionary['event-id'] = event_id
+        low_freq = self.ui.spinBoxLowPass.value()
+        dictionary['ecg-l-freq'] = low_freq
+        high_freq = self.ui.spinBoxHighPass.value()
+        dictionary['ecg-h-freq'] = high_freq
+        grad = self.ui.spinBoxGrad.value()
+        dictionary['n-grad'] = grad
+        mag = self.ui.spinBoxMag.value()
+        dictionary['n-mag'] = mag
+        eeg = self.ui.spinBoxEeg.value()
+        dictionary['n-eeg'] = eeg
+        filter_low = self.ui.spinBoxLow.value()
+        dictionary['l-freq'] = filter_low
+        filter_high = self.ui.spinBoxHigh.value()
+        dictionary['h-freq'] = filter_high
+        rej_grad = self.ui.doubleSpinBoxGradReject.value()
+        dictionary['rej-grad'] = rej_grad
+        rej_mag = self.ui.doubleSpinBoxMagReject.value()
+        dictionary['rej-mag'] = rej_mag
+        rej_eeg = self.ui.doubleSpinBoxEEGReject.value()
+        dictionary['rej-eeg'] = rej_eeg
+        rej_eog = self.ui.doubleSpinBoxEOGReject.value()
+        dictionary['rej-eog'] = rej_eog
+        bads = self.ui.lineEditBad.text()
+        
+        start = self.ui.spinBoxStart.value()
+        dictionary['tstart'] = start
+        taps = self.ui.spinBoxTaps.value()
+        dictionary['filtersize'] = taps
+        njobs = self.ui.spinBoxJobs.value()
+        dictionary['n-jobs'] = njobs
+        eeg_proj = self.ui.checkBoxEEGProj.checkState() == QtCore.Qt.Checked
+        dictionary['avg-ref'] = eeg_proj
+        excl_ssp = self.ui.checkBoxSSPProj.checkState() == QtCore.Qt.Checked
+        dictionary['no-proj'] = excl_ssp
+        comp_ssp = self.ui.checkBoxSSPCompute.checkState()==QtCore.Qt.Checked
+        dictionary['average'] = comp_ssp
+        if self.ui.checkBoxNoECG.checkState() == QtCore.Qt.Checked:
+            dictionary['ch_name'] = self.ui.lineEditECGChannel.text()
+        else:
+            dictionary['ch_name'] = None
+        caller = Caller()
+        caller.call_ecg_ssp(dictionary)
+        self.close()
 
