a99077dfae6effa3ce1a8e25ec8992ce2dfa3d0a
Jaakko Leppäkangas <jaeilepp@sovproj08.it.jyu.fi>
2013-04-23 13:50:34 +0300

    Progress dialog ja stdout
    Former-commit-id: 99166ab3be22f137903bf302dccef700b1abe89a

--- hoksotin/ui/createExperimentDialog_main.py
+++ hoksotin/ui/createExperimentDialog_main.py
@@ -11,16 +11,22 @@ from workspace import Workspace
 #from mainWindow_mainTesti import MainWindow
 from infoDialog_Ui import Ui_infoDialog
 from createExperimentDialog_Ui import Ui_CreateExperimentDialog
+from maxFilterComputeDialog_main import MaxFilterComputeDialog
 
 from PyQt4 import QtCore,QtGui
 # Import the pyuic4-compiled main UI module 
 
 import os,sys
+import StringIO
 import pickle
+import time
+from threading import  Thread
 
 # Create a dialog main window
 class CreateExperimentDialog(QtGui.QDialog):
     fname = ''
+    
+    
     def __init__(self, parent):
         QtGui.QDialog.__init__(self)
         self.fname = ''
@@ -32,20 +38,68 @@ class CreateExperimentDialog(QtGui.QDialog):
         # Refers to class in file CreateProjecDialog
         self.ui = Ui_CreateExperimentDialog() 
         self.ui.setupUi(self)
-        
-        
+        self.ui.showFileInfoButton.setEnabled(False)
+        self.computeDialog = MaxFilterComputeDialog(self, self.parent)
+        """
+        self.computeDialog.open()
+        self.computeDialog.raise_()
+        self.computeDialog.activateWindow()        
+        """
+        QtGui.QApplication.processEvents()
+        self.computeDialog.setVisible(False)
+        sys.stdout = OutLog(self.computeDialog.ui.textEditDetails, sys.stdout)
+                
     def accept(self):
+        self.computeDialog.setVisible(True)
+        self.t2 = Thread(target=self._initialize_experiment())
+        t = Thread(target = self.process_events())
+        
+        t.start()
+        self.t2.start()
+        
+    def on_browseButton_clicked(self, checked=None):
+        if checked is None: return # Standard workaround for file dialog opening twice
+        
+        self.fname = str(QtGui.QFileDialog.getOpenFileName(self, 'Open file',
+                                                           '/home/'))
+        if self.fname != '':
+            try:
+                f = File()
+                self.raw = f.open_raw(self.fname)
+                self.ui.showFileInfoButton.setEnabled(True)
+            except Exception, err:
+                self.messageBox = messageBox.AppForm()
+                self.messageBox.labelException.setText(str(err))
+                self.messageBox.show()
+                
+        self.ui.FilePathLineEdit.setText(self.fname)        
+        
+    def on_showFileInfoButton_clicked(self):
+        try:
+            info = Ui_infoDialog()
+            self.infoDialog = InfoDialog(self.raw, info, True)
+            self.infoDialog.show()
+        except Exception, err:
+            self.messageBox = messageBox.AppForm()
+            self.messageBox.labelException.setText(str(err))
+            self.messageBox.show()
+        QtGui.QApplication.processEvents() 
+        
+    def _initialize_experiment(self):
+        self.k = True
         try:
             if self.ui.lineEditProjectName.text() == '':
-                raise Exception('Give a experiment name!')
+                raise Exception('Give experiment a name!')
             
         except Exception, err:
             self.messageBox = messageBox.AppForm()
             self.messageBox.labelException.setText(str(err))
             self.messageBox.show()
+            self.computeDialog.close()
             return          
-            
-        try: 
+        QtGui.QApplication.processEvents()
+        #self.computeDialog.setValue(10)    
+        try:
             self.workspace = Workspace()
             self.experiment = Experiment()
             self.workspace.working_directory = '/usr/local/bin/'  #'/tmp/' 
@@ -59,8 +113,8 @@ class CreateExperimentDialog(QtGui.QDialog):
             self.messageBox = messageBox.AppForm()
             self.messageBox.labelException.setText("Cannot assign attribute to project")
             self.messageBox.show()
+            self.computeDialog.close()
             return         
-                      
         try:
             # TODO: user should set this workspace from the mainWindow UI    
             self.experiment.save_experiment(self.workspace.working_directory)
@@ -74,8 +128,10 @@ class CreateExperimentDialog(QtGui.QDialog):
             self.messageBox = messageBox.AppForm()
             self.messageBox.labelException.setText(str(err))
             self.messageBox.show()
+            self.computeDialog.close()
             return
-        
+
+        QtGui.QApplication.processEvents()
         self.parent.experiment = self.experiment
         self.parent.raw = self.experiment.raw_data
         InfoDialog(self.parent.experiment.raw_data, self.parent.ui, False)
@@ -87,35 +143,44 @@ class CreateExperimentDialog(QtGui.QDialog):
             item.setText('Trigger ' + str(key) + ', ' + str(value) +
                         ' events')
             self.parent.ui.listWidget.addItem(item)
+        self.k = False
         self.close()
         self.parent._initialize_ui() 
-        
-    def on_browseButton_clicked(self, checked=None):
-        if checked is None: return # Standard workaround for file dialog opening twice
-        
-        self.fname = str(QtGui.QFileDialog.getOpenFileName(self, 'Open file',
-                                                           '/home/'))
-        if self.fname != '':
-            try:
-                f = File()
-                self.raw = f.open_raw(self.fname)
-            except Exception, err:
-                self.messageBox = messageBox.AppForm()
-                self.messageBox.labelException.setText(str(err))
-                self.messageBox.show()
-                
-        self.ui.FilePathLineEdit.setText(self.fname)
-        
-    def on_showFileInfoButton_clicked(self):
-        try:
-            info = Ui_infoDialog()
-            self.infoDialog = InfoDialog(self.raw, info, True)
-            self.infoDialog.show()
-        except Exception, err:
-            self.messageBox = messageBox.AppForm()
-            self.messageBox.labelException.setText(str(err))
-            self.messageBox.show()
 
+        self.computeDialog.close()
+    
+    def process_events(self):
+        self.k=True
+        while self.k:
+            QtGui.QApplication.processEvents()
+        self.computeDialog.setVisible(False)
+        
+class OutLog:
+    def __init__(self, edit, out=None, color=None):
+        """(edit, out=None, color=None) -> can write stdout, stderr to a
+        QTextEdit.
+        edit = QTextEdit
+        out = alternate stream ( can be the original sys.stdout )
+        color = alternate color (i.e. color stderr a different color)
+        """
+        self.edit = edit
+        self.out = None
+        self.color = color
+
+    def write(self, m):
+        if self.color:
+            tc = self.edit.textColor()
+            self.edit.setTextColor(self.color)
+
+        self.edit.moveCursor(QtGui.QTextCursor.End)
+        self.edit.insertPlainText( m )
+
+        if self.color:
+            self.edit.setTextColor(tc)
+
+        if self.out:
+            self.out.write(m)
+    
 """    
 def main(): 
     app = QtGui.QApplication(sys.argv)
---
+++ hoksotin/ui/maxFilterComputeDialog_main.py
@@ -0,0 +1,79 @@
+'''
+Created on Apr 22, 2013
+
+@author: jaeilepp
+'''
+import os
+
+from PyQt4 import QtCore,QtGui
+from maxFilterComputeDialog_Ui import Ui_Dialog
+import messageBox
+from infoDialog_main import InfoDialog
+from infoDialog_Ui import Ui_infoDialog
+
+class MaxFilterComputeDialog(QtGui.QProgressDialog):
+
+
+    def __init__(self, parent, pparent):
+        QtGui.QDialog.__init__(self)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.parent = pparent
+        self.initialize()
+        
+    def initialize(self):
+        """
+        try:
+           if self.parent.ui.lineEditProjectName.text() == '':
+               raise Exception('Give experiment a name!')
+        
+        except Exception, err:
+            self.messageBox = messageBox.AppForm()
+            self.messageBox.labelException.setText(str(err))
+            self.messageBox.show()
+            self.close()
+            return
+        try:
+           self.parent.workspace = Workspace()
+           self.parent.experiment = Experiment()
+           self.parent.workspace.working_directory = '/usr/local/bin/'  #'/tmp/' 
+           self.parent.experiment.author = self.parent.ui.lineEditAuthor.text()
+           self.parent.experiment.experiment_name = self.parent.ui.\
+           lineEditProjectName.text()
+           self.parent.experiment.description = self.parent.ui.textEditDescription.toPlainText()
+
+        except AttributeError:
+            self.messageBox = messageBox.AppForm()
+            self.messageBox.labelException.setText("Cannot assign attribute to project")
+            self.messageBox.show()
+            self.close()
+            return         
+                  
+        try:
+            # TODO: user should set this workspace from the mainWindow UI    
+            self.parent.experiment.save_experiment(self.parent.workspace.working_directory)
+            self.parent.experiment.raw_data = self.parent.raw
+            self.parent.experiment.create_event_set()
+            self.parent.experiment.save_raw(os.path.basename(str(self.parent.ui.FilePathLineEdit.text())))
+            self.parent.experiment.save_experiment_settings()
+      
+        except IOError, err:
+            self.messageBox = messageBox.AppForm()
+            self.messageBox.labelException.setText(str(err))
+            self.messageBox.show()
+            self.close()
+            return
+        
+        self.parent.parent.experiment = self.parent.experiment
+        self.parent.parent.raw = self.parent.experiment.raw_data
+        InfoDialog(self.parent.parent.experiment.raw_data, self.parent.parent.ui, False)
+        self.parent.parent.ui.labelExperimentName.setText(self.parent.experiment.experiment_name)
+        self.parent.parent.ui.listWidget.clear()
+        events = self.parent.experiment.event_set
+        for key, value in events.iteritems():
+            item = QtGui.QListWidgetItem()
+            item.setText('Trigger ' + str(key) + ', ' + str(value) +
+                        ' events')
+            self.parent.parent.ui.listWidget.addItem(item)
+        self.close()
+        """
