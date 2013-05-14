0627b95ff184594628e2d1aa590502bd6af36038
Atte Rautio <atte.m.rautio@student.jyu.fi>
2013-05-14 13:24:27 +0300

    Kommentteja lisatty ja luettavuutta parannettu.
    Former-commit-id: 11d0920fd1aab600f9a62c5fd17f2b33e1e0b262

--- hoksotin/ui/preprocessing/eogParametersDialog_main.py
+++ hoksotin/ui/preprocessing/eogParametersDialog_main.py
@@ -22,46 +22,66 @@ class EogParametersDialog(QtGui.QDialog):
         
     def accept(self):
         dictionary = {'i': self.parent.experiment.working_file}
+        
         tmin = self.ui.doubleSpinBoxTmin.value()
         dictionary['tmin'] = tmin
+        
         tmax = self.ui.doubleSpinBoxTmax.value()
         dictionary['tmax'] = tmax
+        
         event_id = self.ui.spinBoxEventsID.value()
         dictionary['event-id'] = event_id
+        
         low_freq = self.ui.spinBoxLowPass.value()
         dictionary['eog-l-freq'] = low_freq
+        
         high_freq = self.ui.spinBoxHighPass.value()
         dictionary['eog-h-freq'] = high_freq
+        
         grad = self.ui.spinBoxGrad.value()
         dictionary['n-grad'] = grad
+        
         mag = self.ui.spinBoxMag.value()
         dictionary['n-mag'] = mag
+        
         eeg = self.ui.spinBoxEeg.value()
         dictionary['n-eeg'] = eeg
+        
         filter_low = self.ui.spinBoxLow.value()
         dictionary['l-freq'] = filter_low
+        
         filter_high = self.ui.spinBoxHigh.value()
         dictionary['h-freq'] = filter_high
+        
         rej_grad = self.ui.doubleSpinBoxGradReject.value()
         dictionary['rej-grad'] = rej_grad
+        
         rej_mag = self.ui.doubleSpinBoxMagReject.value()
         dictionary['rej-mag'] = rej_mag
+        
         rej_eeg = self.ui.doubleSpinBoxEEGReject.value()
         dictionary['rej-eeg'] = rej_eeg
+        
         rej_eog = self.ui.doubleSpinBoxEOGReject.value()
         dictionary['rej-eog'] = rej_eog
+        
         bads = self.ui.lineEditBad.text()
         
         start = self.ui.spinBoxStart.value()
         dictionary['tstart'] = start
+        
         taps = self.ui.spinBoxTaps.value()
         dictionary['filtersize'] = taps
+        
         njobs = self.ui.spinBoxJobs.value()
         dictionary['n-jobs'] = njobs
+        
         eeg_proj = self.ui.checkBoxEEGProj.checkState() == QtCore.Qt.Checked
         dictionary['avg-ref'] = eeg_proj
+        
         excl_ssp = self.ui.checkBoxSSPProj.checkState() == QtCore.Qt.Checked
         dictionary['no-proj'] = excl_ssp
+        
         comp_ssp = self.ui.checkBoxSSPCompute.checkState()==QtCore.Qt.Checked
         dictionary['average'] = comp_ssp
         
