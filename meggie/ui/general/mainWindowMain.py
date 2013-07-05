ced931c5961386dae65d883913869e1f8a170cb2
Janne Pesonen <jaolpeso@sovproj07.it.jyu.fi>
2013-07-05 14:21:21 +0300

    Turhuutta
    Former-commit-id: 6a7c764b098c84ab35f2ce220e1c1aca75d3a0ad

--- meggie/ui/general/mainWindowMain.py
+++ meggie/ui/general/mainWindowMain.py
@@ -218,8 +218,6 @@ class MainWindow(QtGui.QMainWindow):
         epoch = self.widget.ui.listWidgetEpochs.currentItem().\
         data(32).toPyObject()
         evoked = epoch.average()
-        #evoked.save(self.experiment.raw_data.info.get('filename') + '.fif')
-        #evoked.save('/home/jaolpeso/sample_audvis_eeg-ave.fif')
          
     def on_pushButtonMNE_Browse_Raw_clicked(self, checked=None):
         """
