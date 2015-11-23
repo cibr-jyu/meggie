68517862492959be7d10ef9c136cccfd4046422e
Erkka Heinila <erkkahe@gmail.com>
2015-11-23 18:18:27 +0200

    Add setup.py to make this python package
    Former-commit-id: a390b330ee6cc696ff12c4dbcadfeadd00d8db56

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='meggie',
+    version='0.1.0',
+    description="",
+    author='Erkka Heinila',
+    author_email='erkka.heinila@jyu.fi',
+    url='https://yousource.it.jyu.fi/+hoksotin/hoksotin/lahdekoodit',
+    license='GPL',
+    packages=['meggie'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'mne==0.10.0',
+    ],
+    entry_points = {
+        'console_scripts': ['meggie=meggie.run:main'],
+    },
+)
