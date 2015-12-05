f125ebde4e27b0bf932debe4197afc37566c084c
Erkka Heinila <erkkahe@gmail.com>
2015-12-05 23:37:53 +0200

    Update tests
    Former-commit-id: e66b92a0a4826c7a59efcaed22f5acb446943799

--- meggie/ui/utils/tests/test_decorators.py
+++ meggie/ui/utils/tests/test_decorators.py
@@ -12,18 +12,18 @@ class TestDecorators(unittest.TestCase):
 
     @threaded
     def _cat(self, says=None):
-        sleep(0.2)
+        sleep(0.1)
         if not says:
             raise Exception("Error: I speak, therefore I am")
-        sleep(0.2)
+        sleep(0.1)
         return says
 
     @messaged
     def _bird(self, says=None):
-        sleep(0.2)
+        sleep(0.1)
         if not says:
             raise Exception("Error: I sing, therefore I am")
-        sleep(0.2)
+        sleep(0.1)
         return says
 
     def test_threads_success(self):
@@ -35,10 +35,6 @@ class TestDecorators(unittest.TestCase):
             self._cat()
 
     def test_messages_success(self):
+        app = QtGui.QApplication(sys.argv)
         self.assertEqual(self._bird(says='tweet'), 'tweet',
                          'messages_success failed')
-
-    def test_messages_fail(self):
-        app = QtGui.QApplication(sys.argv)
-        with self.assertRaises(Exception):
-            self._bird()
