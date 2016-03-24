cb41065605c0cca004674c9adbf90392b6d32462
Erkka Heinila <erkkahe@gmail.com>
2016-03-25 00:42:01 +0200

    Fix tests
    Former-commit-id: d273216eaad84a82241eecc7925b0a8ddb7ddf8f

--- meggie/code_meggie/general/tests/test_caller.py
+++ meggie/code_meggie/general/tests/test_caller.py
@@ -36,21 +36,18 @@ tempdir = _TempDir()
 
 def _get_experiment():
     """Helper for constructing experiment."""
-    raise Exception("Has to be rewritten")
     exp = experiment.Experiment()
+    exp.experiment_name = 'test'
     exp.workspace = tempdir._path
     os.chmod(tempdir._path, 0777)
-    exp.name = 'test'
-    sub = subject.Subject(exp, 'test_sub')
-    sub._working_file = Raw(fname, preload=True)
+    sub = subject.Subject(exp, 'test_sub', 'test_sub.fif')
+    sub.set_working_file(Raw(fname, preload=True))
     exp.add_subject(sub)
-    path = os.path.join(tempdir._path, 'experiment')
-    os.mkdir(path)
-    #sub.save_raw(fname, sub.subject_path)
-    fileManager.save_subject(experiment, sub, fname, sub.subject_path)
-    exp.active_subject_name = sub.subject_name
-    exp.update_working_file(exp.active_subject_name, sub.subject_name)
+    exp.active_subject = sub
+    os.makedirs(sub.subject_path)
     experiment.ExperimentHandler(caller.parent).initialize_logger(exp)
+    fileManager.save_raw(exp, sub.get_working_file(), sub.working_file_path)
+    exp.active_subject = sub
     return exp
 
 
@@ -69,7 +66,7 @@ def _update_ui():
 def test_call_exg_ssp():
     """Test calling ecg_ssp and eog_ssp."""
     _setup_caller()
-    raw = caller.experiment.subjects[0].get_working_file()
+    raw = caller.experiment.active_subject.get_working_file()
     params = {'i': raw, 'tmin': -0.1, 'tmax': 0.1, 'event-id': 999,
               'ecg-l-freq': 1., 'ecg-h-freq': 50., 'n-grad': 1, 'n-mag': 1,
               'n-eeg': 1, 'l-freq': 0, 'h-freq': 50, 'rej-grad': 2000,
@@ -80,13 +77,13 @@ def test_call_exg_ssp():
 
     caller._call_ecg_ssp(
         params, 
-        caller.experiment.get_subjects()[0],
+        caller.experiment.active_subject,
         do_meanwhile=_update_ui,
     )
 
     params.update({'eog-l-freq': 1., 'eog-h-freq': 40.})
     result = caller._call_eog_ssp(
         params, 
-        caller.experiment.get_subjects()[0],
+        caller.experiment.active_subject,
         do_meanwhile=_update_ui,
     )
