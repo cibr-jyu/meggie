Remake epoch masking, fix raw plot to not save when bad channels didnt change
Former-commit-id: 6cbc1e1ebaca029ecdd7773881bb5abd62437654

meggie/ui/preprocessing/addECGProjectionsMain.py
@@ -34,9 +34,13 @@ Created on Apr 25, 2013
 Contains the AddECGProjections-class used for adding ECG projections.
 """
 
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+
 import glob
 import mne
 import numpy as np
+from pylab import get_current_fig_manager
+
 
 from PyQt4 import QtCore,QtGui
 
@@ -95,7 +99,8 @@ class AddECGProjections(QtGui.QDialog):
             applied = np.array(applied)
 
         raw.add_proj(self.projs[applied])
-        raw.plot()
+        fig = raw.plot()
+        #self.hide()
         
     def create_applied_list(self):
         applied = list()

meggie/ui/preprocessing/addEOGProjectionsMain.py
@@ -36,6 +36,7 @@ Contains the AddEOGProjections-class used for adding EOG projections.
 
 import glob
 import mne
+import numpy as np
 
 from PyQt4 import QtCore,QtGui
 
@@ -77,14 +78,39 @@ class AddEOGProjections(QtGui.QDialog):
             if str(proj) in [str(x) for x in added_projs]:
                 checkBox.setChecked(True)
 
+    def on_pushButtonPreview_clicked(self, checked=None):
+        if checked is None:
+            return
+        
+        raw = self.caller.experiment.active_subject.get_working_file()
+        applied = self.create_applied_list()
+   
+        raw = raw.copy()
+
+        raw.apply_proj()
+        raw.info['projs'] = []
+        
+        if not isinstance(self.projs, np.ndarray):
+            self.projs = np.array(self.projs)
+        if not isinstance(applied, np.ndarray):
+            applied = np.array(applied)
+
+        raw.add_proj(self.projs[applied])
+        raw.plot()
+        
+    def create_applied_list(self):
+        applied = list()
+        
+        for index in xrange(self.listWidget.count()):
+            check_box=self.listWidget.itemWidget(self.listWidget.item(index))
+            applied.append(check_box.isChecked())
+        return applied
+
     def accept(self):
         """
         Adds the projections.
         """
-        applied = list()
-        for index in xrange(self.listWidget.count()):
-            check_box = self.listWidget.itemWidget(self.listWidget.item(index))
-            applied.append(check_box.isChecked())
+        applied = self.create_applied_list()
 
         raw = self.caller.experiment.active_subject.get_working_file()
         directory = self.caller.experiment._active_subject._subject_path