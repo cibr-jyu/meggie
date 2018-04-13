4dca9bf2b547d6089e208e5d83fc61ff00858c32
Erkka Heinila <erkkahe@gmail.com>
2018-04-13 13:25:19 +0300

    Add spectrum and tfr object mockups
    Former-commit-id: 94322873baa48ef8a3f74d281baf69b6dbcc81c3

---
+++ meggie/code_meggie/structures/tfr.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+"""
+"""
+
+import os
+
+import meggie.code_meggie.general.mne_wrapper as mne
+
+class TFR(object):
+    
+    """
+    A class for creating and handling spectrums
+    
+    """
+
+    def __init__(self):
+        """
+        """
