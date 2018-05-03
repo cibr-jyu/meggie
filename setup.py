e5349c0e425aa67c5a2a63a9b44b4ba3111b90f9
Erkka Heinila <erkkahe@gmail.com>
2018-05-03 15:20:57 +0300

    Release v0.8.1
    Former-commit-id: 074ef5d14f50ee41dab84b9518a8e55a6765dbf3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='meggie',
-    version='0.8.0',
+    version='0.8.1',
     description="",
     author='CIBR',
     author_email='erkka.heinila@jyu.fi',
