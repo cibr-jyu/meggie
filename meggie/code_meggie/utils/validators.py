7a4fe555ff6f412dab66adfd441d13af4887a051
Erkka Heinila <erkkahe@gmail.com>
2019-01-31 17:55:47 +0200

    Update validator
    Former-commit-id: 6c9e2e7266b73b1aec84e6cbcd4b910612f1bab1

--- meggie/code_meggie/utils/validators.py
+++ meggie/code_meggie/utils/validators.py
@@ -11,7 +11,7 @@ def validate_name(name, minlength=1, maxlength=30, fieldname='name'):
         raise Exception('Too long ' + fieldname + ' (over ' + str(maxlength) + 
                         ' characters.)')
 
-    if not re.match(r'^[A-Za-z0-9_]+$', name):
+    if not re.match(r'^[A-Za-z0-9_]*$', name):
         raise Exception(fieldname + ' can only contain alphanumeric ' + 
                         'characters or underscores')
 
