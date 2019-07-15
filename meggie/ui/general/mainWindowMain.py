d8ad9f4fab4624b054a66476387098a9510237bf
Erkka Heinila <erkkahe@gmail.com>
2019-07-15 22:00:36 +0300

    Handle button presses

--- meggie/ui/general/mainWindowMain.py
+++ meggie/ui/general/mainWindowMain.py
@@ -2,6 +2,7 @@
 
 """
 """
+import importlib
 
 from meggie.code_meggie.utils.units import get_unit
 from meggie.code_meggie.general import fileManager
@@ -114,9 +115,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
                     # then fill the contents
 
-                    for idx, input_item in enumerate(tab_spec.get('inputs', [])):
+                    for idx, input_element in enumerate(tab_spec.get('inputs', [])):
                         groupBoxInputElement = QtWidgets.QGroupBox(self.groupBoxInputs)
-                        groupBoxInputElement.setTitle(input_item.capitalize())
+                        groupBoxInputElement.setTitle(input_element.capitalize())
                         gridLayoutInputElement = QtWidgets.QGridLayout(groupBoxInputElement)
                         listWidgetInputElement = QtWidgets.QListWidget(groupBoxInputElement)
 
@@ -130,9 +131,9 @@ class MainWindow(QtWidgets.QMainWindow):
                         setattr(self, 'listWidgetInputElement_' + str(idx+1), 
                                 listWidgetInputElement)
 
-                    for idx, output_item in enumerate(tab_spec.get('outputs', [])):
+                    for idx, output_element in enumerate(tab_spec.get('outputs', [])):
                         groupBoxOutputElement = QtWidgets.QGroupBox(self.groupBoxOutputs)
-                        groupBoxOutputElement.setTitle(output_item.capitalize())
+                        groupBoxOutputElement.setTitle(output_element.capitalize())
                         gridLayoutOutputElement = QtWidgets.QGridLayout(groupBoxOutputElement)
                         listWidgetOutputElement = QtWidgets.QListWidget(groupBoxOutputElement)
 
@@ -146,19 +147,51 @@ class MainWindow(QtWidgets.QMainWindow):
                         setattr(self, 'listWidgetOutputElement_' + str(idx+1), 
                                 listWidgetOutputElement)
 
-                    for idx, transform_item in enumerate(tab_spec.get('transforms', [])):
+                    # connect to handler
+                    def connect_to_handler(button, element):
+                        module_name = tab_spec['id']
+                        try:
+                            module = importlib.import_module('meggie.ui.general.tabs.' +
+                                                             module_name)
+                            handler = getattr(module, element)
+
+                            def handler_wrapper(checked):
+                                experiment = self.parent.experiment
+                                if not experiment:
+                                    return
+
+                                subject = experiment.active_subject
+                                if not subject:
+                                    return
+
+                                inputs = []
+
+                                handler(subject, inputs)
+                                
+                            if handler:
+                                button.clicked.connect(handler_wrapper)
+                        except ModuleNotFoundError:
+                            pass
+                        except AttributeError:
+                            pass
+
+                    for idx, transform_element in enumerate(tab_spec.get('transforms', [])):
                         pushButtonTransformElement = QtWidgets.QPushButton(self.groupBoxTransforms)
-                        pushButtonTransformElement.setText(transform_item.capitalize())
+                        pushButtonTransformElement.setText(transform_element.capitalize())
                         self.gridLayoutTransforms.addWidget(pushButtonTransformElement, idx, 0, 1, 1)
                         setattr(self, 'pushButtonTransformElement_' + str(idx+1), 
                                 pushButtonTransformElement)
 
-                    for idx, action_item in enumerate(tab_spec.get('actions', [])):
+                        connect_to_handler(pushButtonTransformElement, transform_element)
+
+                    for idx, action_element in enumerate(tab_spec.get('actions', [])):
                         pushButtonActionElement = QtWidgets.QPushButton(self.groupBoxActions)
-                        pushButtonActionElement.setText(action_item.capitalize())
+                        pushButtonActionElement.setText(action_element.capitalize())
                         self.gridLayoutActions.addWidget(pushButtonActionElement, idx, 0, 1, 1)
                         setattr(self, 'pushButtonActionElement_' + str(idx+1), 
                                 pushButtonActionElement)
+
+                        connect_to_handler(pushButtonActionElement, action_element)
 
                     if tab_spec.get('inputs') and not tab_spec.get('transforms'):
                         self.gridLayoutRoot.addWidget(self.groupBoxInputs, 0, 0, 2, 1)
@@ -187,7 +220,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
 
                 def initialize_ui(self):
-                    pass
+                    logging.getLogger('ui_logger').info('Updating lists..')
 
             DynamicTab.__name__ = 'MainWindowTab' + tab_spec['id'].capitalize()
 
@@ -464,16 +497,16 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.initialize_ui()
 
-    def get_epochs(self, epoch_name):
-        experiment = self.experiment
-        if not experiment:
-            return
+    # def get_epochs(self, epoch_name):
+    #     experiment = self.experiment
+    #     if not experiment:
+    #         return
 
-        active_subject = experiment.active_subject
-        if not active_subject:
-            return
+    #     active_subject = experiment.active_subject
+    #     if not active_subject:
+    #         return
 
-        return active_subject.epochs.get(epoch_name)
+    #     return active_subject.epochs.get(epoch_name)
 
     def update_ui(self):
         """
---
+++ meggie/ui/general/tabs/preprocessing.py
@@ -0,0 +1,2 @@
+def plot(subject, inputs):
+    print("Plot success for " + subject.subject_name)
