3158aae30838e7c83605921ea2106b10481d72a4
Erkka Heinila <erkkahe@gmail.com>
2019-09-19 19:22:30 +0300

    Prepare for spectrum and tfr tabs

---
+++ meggie/tabs/spectrum/ui.py
@@ -0,0 +1,45 @@
+
+
+def spectrum_info(experiment, data, window):
+    return ""
+
+
+def group_average(experiment, data, window):
+    pass
+
+
+def create(experiment, data, window):
+    pass
+
+
+def delete(experiment, data, window):
+    pass
+
+
+def delete_from_all(experiment, data, window):
+    pass
+
+
+def plot_spectrum(experiment, data, window):
+    pass
+
+
+def plot_averages(experiment, data, window):
+    pass
+
+
+def save(experiment, data, window):
+    pass
+
+
+def save_from_all(experiment, data, window):
+    pass
+
+
+def save_averages(experiment, data, window):
+    pass
+
+
+def save_averages_from_all(experiment, data, window):
+    pass
+
---
+++ meggie/tabs/tfr/ui.py
@@ -0,0 +1,16 @@
+
+
+def create(experiment, data, window):
+    pass
+
+
+def plot_tfr(experiment, data, window):
+    pass
+
+
+def plot_tse(experiment, data, window):
+    pass
+
+
+def tfr_info(experiment, data, window):
+    pass
