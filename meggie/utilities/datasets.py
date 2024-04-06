25233a603e4dbcb6ea156bafe31f3be586cc9ef5
teekuningas <erkkahe@gmail.com>
2024-04-06 12:31:50 +0300

    Add somato dataset

--- meggie/utilities/datasets.py
+++ meggie/utilities/datasets.py
@@ -58,8 +58,39 @@ def create_limo_experiment(name, author, prefs, set_previous_experiment=True):
     return experiment
 
 
-def create_sample_experiment(name, author, prefs, set_previous_experiment=True):
+def create_somato_experiment(name, author, prefs, set_previous_experiment=True):
     """Generate one-subject experiment based on the mne sample data.
+
+    Parameters
+    ----------
+    name : str
+        Name of the experiment.
+    author : str
+        Name of the author.
+    prefs : meggie.mainwindow.preferences.PreferencesHandler
+        A preferences object.
+
+    Returns
+    -------
+    meggie.experiment.Experiment
+        The new experiment.
+    """
+
+    experiment = initialize_new_experiment(name, author, prefs, set_previous_experiment)
+
+    mne.datasets.somato.data_path()
+
+    somato_data = (
+        str(mne.datasets.somato.data_path()) + "/sub-01/meg/sub-01_task-somato_meg.fif"
+    )
+
+    experiment.create_subject("sub-01", somato_data)
+
+    return experiment
+
+
+def create_sample_experiment(name, author, prefs, set_previous_experiment=True):
+    """Generate one-subject experiment based on the mne somato data.
 
     Parameters
     ----------
@@ -88,6 +119,13 @@ def create_sample_experiment(name, author, prefs, set_previous_experiment=True):
 
 def get_open_datasets():
     return {
-        "limo": {"title": "LIMO Dataset", "constructor": create_limo_experiment},
-        "sample": {"title": "MNE Sample Data", "constructor": create_sample_experiment},
+        "limo": {"title": "MNE LIMO Dataset", "constructor": create_limo_experiment},
+        "somato": {
+            "title": "MNE Somato Dataset",
+            "constructor": create_somato_experiment,
+        },
+        "sample": {
+            "title": "MNE Sample Dataset",
+            "constructor": create_sample_experiment,
+        },
     }
