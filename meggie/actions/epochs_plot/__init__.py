be35088fd3ae6cc1f745a4fe1c57afd8a02b5ed5
teekuningas <erkkahe@gmail.com>
2025-04-12 16:08:06 +0300

    Update epochs_plot and raw_plot to properly save bads, annotations and selections in subject_action to ensure reproducibility

--- meggie/actions/epochs_plot/__init__.py
+++ meggie/actions/epochs_plot/__init__.py
@@ -1,7 +1,12 @@
 """Contains implementation for epochs plot"""
 
+import logging
+
+import matplotlib.pyplot as plt
+
 from meggie.utilities.messaging import exc_messagebox
 from meggie.mainwindow.dynamic import Action
+from meggie.mainwindow.dynamic import subject_action
 
 
 class PlotEpochs(Action):
@@ -15,12 +20,52 @@ class PlotEpochs(Action):
             return
 
         subject = self.experiment.active_subject
-        try:
-            self.handler(subject, {"name": selected_name})
-        except Exception as exc:
-            exc_messagebox(self.window, exc)
+        epochs = subject.epochs.get(selected_name)
 
+        # Create a copy to not drop epochs from the original yet
+        mne_epochs = epochs.content.copy()
+
+        old_selection = mne_epochs.selection.copy()
+
+        def handle_close(event):
+            selection_changed = len(old_selection) != len(mne_epochs.selection)
+
+            params = {}
+            params["name"] = selected_name
+            params["selection_changed"] = selection_changed
+            params["selection"] = mne_epochs.selection
+
+            try:
+                if selection_changed:
+                    self.handler(subject, params)
+                    self.window.initialize_ui()
+            except Exception as exc:
+                exc_messagebox(self.window, exc)
+
+        fig = mne_epochs.plot(show=False)
+        fig.canvas.mpl_connect("close_event", handle_close)
+        plt.show()
+
+    @subject_action
     def handler(self, subject, params):
+
+        logging.getLogger("ui_logger").info("Epochs dropped!")
+
         epochs = subject.epochs.get(params["name"])
+
+        # Finally, drop the epochs
         mne_epochs = epochs.content
-        mne_epochs.plot()
+        dropped = [
+            sel_idx
+            for sel_idx in mne_epochs.selection
+            if sel_idx not in params["selection"]
+        ]
+        dropped_idxs = [
+            idx
+            for idx, sel_idx in enumerate(mne_epochs.selection)
+            if sel_idx in dropped
+        ]
+        mne_epochs.drop(dropped_idxs)
+
+        # And save
+        epochs.save_content()
--- meggie/actions/raw_plot/__init__.py
+++ meggie/actions/raw_plot/__init__.py
@@ -5,6 +5,8 @@ import logging
 import matplotlib.pyplot as plt
 import numpy as np
 
+import mne
+
 from meggie.utilities.events import find_stim_channel
 from meggie.utilities.events import find_events
 from meggie.utilities.messaging import exc_messagebox
@@ -24,6 +26,9 @@ class PlotRaw(Action):
         raw = subject.get_raw()
         if not raw:
             return
+
+        # Create a copy to not update the original yet
+        raw = raw.copy()
 
         old_bads = raw.info["bads"].copy()
         old_annotations = raw.annotations.copy()
@@ -45,10 +50,20 @@ class PlotRaw(Action):
                 annotations_changed = True
 
             params = {}
-            params["bads"] = raw.info["bads"]
-            params["annotations"] = list(raw.annotations)
             params["bads_changed"] = bads_changed
+            params["bads"] = [str(bad) for bad in raw.info["bads"]]
+
             params["annotations_changed"] = annotations_changed
+
+            params["annotations"] = []
+            for idx in range(len(raw.annotations)):
+                params["annotations"].append(
+                    {
+                        "onset": raw.annotations.onset[idx],
+                        "duration": raw.annotations.duration[idx],
+                        "description": raw.annotations.description[idx],
+                    }
+                )
 
             try:
                 # only run logged action if there are changes
@@ -70,5 +85,23 @@ class PlotRaw(Action):
         if params["annotations_changed"]:
             logging.getLogger("ui_logger").info("Annotations changed!")
 
-        if params["bads_changed"] or params["annotations_changed"]:
-            subject.save()
+        raw = subject.get_raw()
+
+        # Finally, update the raw based on new params. First annotations.
+        onset = []
+        duration = []
+        description = []
+        for annotation_params in params["annotations"]:
+            # set_annotations adds the first_samp based offset, so take that into account.
+            onset.append(
+                annotation_params["onset"] - raw.first_samp / raw.info["sfreq"]
+            )
+            duration.append(annotation_params["duration"])
+            description.append(annotation_params["description"])
+        annotations = mne.Annotations(onset, duration, description)
+        raw.set_annotations(annotations)
+
+        # Then the bad channels.
+        raw.info["bads"] = params["bads"]
+
+        subject.save()
